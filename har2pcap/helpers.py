4ecc5f1d6ad9def0e63c649d6532b2044ef4e0b3
Jonas Matter <jonas.matter@hsr.ch>
2016-06-05 15:39:51 +0200

    remove obsolote method

--- har2pcap/helpers.py
+++ har2pcap/helpers.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-from subprocess import Popen, PIPE
-import re
 import struct
-
-
-def resolve_mac(ip):
-    Popen(["ping", "-c 1", ip], stdout=PIPE)
-    pid = Popen(["arp", "-n", ip], stdout=PIPE)
-    s = pid.communicate()[0]
-    mac = re.search(b'(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})', s).groups()[0]
-    return mac.decode('UTF-8')
     
 def encode_mac(mac):
     mac = mac.split(':')
