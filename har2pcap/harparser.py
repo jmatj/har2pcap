a264b023b5bcfef5550100d7c130e63681b50d55
Jonas Matter <jonas.matter@hsr.ch>
2016-06-05 14:43:46 +0200

    build http packets in harparser

--- har2pcap/harparser.py
+++ har2pcap/harparser.py
@@ -1,7 +1,6 @@
 import json
-import blocks
 import datetime
-
+from packet import (EthPacket, IPv4Packet, TCPPacket, HTTPPacket, HTTPRequestPacket, HTTPResponsePacket, PacketBuilder)
 
 def parse_har(url):
     with open(url) as json_file:
@@ -12,13 +11,40 @@ def build_blocks(har):
     """Return a list of HTTP blocks"""
     blocks = []
     for entry in har['entries']:
-        start_time = datetime.datetime.strptime(
-            entry['startedDateTime'], '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()
+        start_time = datetime.datetime.strptime(entry['startedDateTime'], '%Y-%m-%dT%H:%M:%S.%fZ').timestamp()
         timestamp = start_time * 1e6 + int(entry['time'] * 1e3)
-        request = entry['request']
-        response = entry['response']
-        # TODO: Build http-packet from request and response
+        block = {
+            'timestamp': timestamp,
+            'request': build_request_block(entry['request']),
+            'response': build_response_block(entry['response'])
+        }
+        blocks.append(block)
     return blocks
+
+
+def build_request_block(packet):
+    eth_packet = EthPacket('ab:cd:ef:12:34:56', '65:43:21:fe:dc:ba')
+    ip_packet = IPv4Packet('192.168.0.1', '8.8.8.8')
+    tcp_packet = TCPPacket(34567, 80)
+
+    http_headers = packet['headers']
+    http_content = ''
+    http_packet = HTTPRequestPacket(packet['method'], packet['url'], packet['httpVersion'], http_headers, http_content)
+    builder = PacketBuilder(eth_packet, ip_packet, tcp_packet, http_packet)
+    return builder
+
+
+def build_response_block(packet):
+    eth_packet = EthPacket('65:43:21:fe:dc:ba', 'ab:cd:ef:12:34:56')
+    ip_packet = IPv4Packet('8.8.8.8', '192.168.0.1')
+    tcp_packet = TCPPacket(34567, 80)
+
+    http_headers = packet['headers']
+    http_content = packet['content']['text']
+    http_packet = HTTPResponsePacket(packet['status'], packet['statusText'], packet['httpVersion'], http_headers, http_content)
+    builder = PacketBuilder(eth_packet, ip_packet, tcp_packet, http_packet)
+    return builder
+
 
 if __name__ == "__main__":
     har = parse_har('../example.org.har')
